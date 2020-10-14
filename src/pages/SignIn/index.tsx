63afa4a66a2a126c289313dd6b8af7e31eed1941
Your Name <bsromeiro@gmail.com>
2020-10-13 23:17:42 -0300

    core | first page layout

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+
+const SignIn: React.FC = () => (
+  <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        contentContainerStyle={{ flex: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Container>
+          <Image source={logoImg} />
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button
+            onPress={() => {
+              console.log('Olá');
+            }}
+          >
+            Entrar
+          </Button>
+
+          <ForgotPassword
+            onPress={() => {
+              console.log('Hi');
+            }}
+          >
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <CreateAccountButton
+      onPress={() => {
+        console.log('Hi');
+      }}
+    >
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+    </CreateAccountButton>
+  </>
+);
+
+export default SignIn;
